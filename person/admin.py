24071af0a38c4f65106946dcf5ce40fa0f6346d7
hancush <hannah.cushman@gmail.com>
2021-07-14 10:31:13 -0500

    Remove model translation UI

--- person/admin.py
+++ person/admin.py
@@ -1,7 +1,6 @@
 import reversion
 
 from django.contrib import admin
-from modeltranslation.admin import TranslationAdmin
 
 from .models import Person, PersonName, PersonAlias
 
@@ -10,14 +9,9 @@ class PersonAdmin(admin.ModelAdmin):
     pass
 
 
-class PersonNameAdmin(TranslationAdmin):
-    group_fieldsets = True
-
-
 class PersonAliasAdmin(reversion.admin.VersionAdmin):
     pass
 
 
 admin.site.register(Person, PersonAdmin)
-admin.site.register(PersonName, PersonNameAdmin)
 admin.site.register(PersonAlias, PersonAliasAdmin)
